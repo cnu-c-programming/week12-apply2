174fa8656b98c7a608fda671ebf71fe7cff85881
apply2 <67138665+apply2@users.noreply.github.com>
2026-05-27 16:30:51 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2)
-        return 0;
+    if (argc < 2) return 0;
 
     FILE* fp = fopen(argv[1], "r");
 
-
+    int arr[26] = {0};
+    
+    int idx;
+    char c;
+    while ((c = fgetc(fp))!= EOF)
+    {
+        if (c >= 'a' && c <= 'z')
+        {
+            idx = c - 'a';
+            arr[idx]++;
+        }
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        printf("%c: %d\n", 'a' + i, arr[i]);
+    }
 
     fclose(fp);
 }
